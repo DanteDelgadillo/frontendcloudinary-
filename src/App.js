950037128d6d2e23c9fb33ff2e6db9c93d5784b4
dante <40077234+DanteDelgadillo@users.noreply.github.com>
2019-10-26 19:02:40 -0700

    update

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import React from 'react';
 
 import './App.css'
 import NavBar from "./Components/navBar";
-import { BrowserRouter } from "react-router-dom";
-import Main from "./Components/main";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+// import Main from "./Components/main";
+import Upload from "./Components/upload"
+import imageshomepage from "./Components/imageloading";
+
 
 function App() {
   return (
     <React.Fragment >
       <NavBar />
-      <BrowserRouter>
-        <div>
+      <Router>
+        {/* <div>
           <Main />
-        </div>
-      </BrowserRouter>
+        </div> */}
+        <Route exact path="/upload" component={Upload} />
+        <Route exact path="/" component={imageshomepage} />
+      </Router>
     </React.Fragment>
   );
 }
